250c3a7fbcbfcd22fe24297f6ac0d7c44febd3b1
Magnus Melin <qfunction@gmail.com>
2019-02-03 12:36:06 +0100

    Add a simple index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+	/**
+	* @author Magnus Melin
+    * @copyright Magnus Melin 2019
+    * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+    */
+
+	echo'
+	<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+    <html>
+    	<head>
+    		<title>Qfunction</title>
+			<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+			<meta name="author" content="Qfunction - Magnus Melin">
+        </head>
+		<body>
+			<h1 style="font-size: 200px; font-weight: normal; text-align: center; margin-top: 200px;">qf</h1>
+		</body>
+	</html>';
+?>
